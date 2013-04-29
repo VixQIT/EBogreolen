96927130e41cf1f71b08aea0578cd424b3c0c9a7
hallundbaek <tobias.h.petersen@gmail.com>
2013-04-29 15:25:03 +0200

    Setup of android app environment, and added first page

---
+++ bin/classes/com/example/ebogreolen/Search.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Search {
+
+  public static final Search instance = new Search();
+
+  private String searchString = null;
+  
+  public Search() {
+  }
+
+  public ArrayList<Integer> getResults(){
+    if (searchString != null) {
+      return WebAPIIO.instance.getResults(searchString);
+    } else {
+      return null;
+    }
+  }
+
+  public void setSearchString(String searchString){
+    this.searchString = searchString;
+  }
+  
+  public String getSearchString() {
+    return this.searchString;
+  }
+
+  
+}
---
+++ src/com/example/ebogreolen/YourBooks.java
@@ -0,0 +1,16 @@
+package com.example.ebogreolen;
+
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+
+public class YourBooks extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_books);
+        View rowOne = findViewById(R.id.tableRow1);
+    }
+    
+}
