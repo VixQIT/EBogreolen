0dd7a7751efa1eab53e6ec04d769cad52439e289
Vix <Nikolaj.D.Rathcke@gmail.com>
2013-05-28 19:58:43 +0200

    added Results activity

--- gen/com/example/ebogreolen/R.java
+++ gen/com/example/ebogreolen/R.java
@@ -10,6 +10,16 @@ package com.example.ebogreolen;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int backrepeat=0x7f020001;
@@ -21,23 +31,29 @@ public final class R {
         public static final int testcover2=0x7f020007;
     }
     public static final class id {
-        public static final int bookTable=0x7f070002;
-        public static final int bookView=0x7f070001;
-        public static final int button1=0x7f070003;
-        public static final int listview_background_shape=0x7f070004;
-        public static final int logo=0x7f070000;
-        public static final int menu_settings=0x7f070005;
+        public static final int action_settings=0x7f080006;
+        public static final int bookTable=0x7f080002;
+        public static final int bookView=0x7f080001;
+        public static final int button1=0x7f080003;
+        public static final int listview_background_shape=0x7f080004;
+        public static final int logo=0x7f080000;
+        public static final int menu_settings=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_your_books=0x7f030000;
+        public static final int activity_results=0x7f030000;
+        public static final int activity_your_books=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_your_books=0x7f060000;
+        public static final int activity_your_books=0x7f070000;
+        public static final int results=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int logo=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int action_settings=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050005;
+        public static final int logo=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_results=0x7f050003;
     }
     public static final class style {
         /** 
@@ -61,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
