f61f9af4aa0710510934c087e9f970ad2b10797c
Vix <Nikolaj.D.Rathcke@gmail.com>
2013-05-28 22:01:11 +0200

    ingenting

--- src/com/example/ebogreolen/Results.java
+++ src/com/example/ebogreolen/Results.java
@@ -1,8 +1,13 @@
 package com.example.ebogreolen;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class Results extends Activity {
 
@@ -10,13 +15,17 @@ public class Results extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_results);
-	}
+		
+		ArrayList<TableRow> rows = new ArrayList<TableRow>();
+		TableLayout.LayoutParams rowLP = new TableLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT, 0);
+		rowLP.weight = 1;
+		for (int i = 0; i < 51; i++) {
+			TableRow row = new TableRow(this);
+			row.setId(i * 100);
+			row.setLayoutParams(rowLP);
+			rows.add(row);
+		}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.results, menu);
-		return true;
 	}
-
 }
